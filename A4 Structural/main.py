5bf7182a2a720aafeb5473637fbe13ec0fe00b9e
Flotfar <113911826+Flotfar@users.noreply.github.com>
2022-11-21 14:58:36 +0100

    Update main.py

--- A4 Structural/main.py
+++ A4 Structural/main.py
@@ -68,60 +68,98 @@ slab_num = []
 
 
 ######## SLABS:
+# Creating empty property vectors:
+slabs_structural = [] ; slab_num = [] ; slab_El_name = [] ; slab_Perimeter = [] ; slab_Material = []
+slab_Area = [] ; slab_Volume = [] ; slab_Thickness = [] ; slab_Level = []
+slab_xval = [] ; slab_yval = [] ; slab_zval = [] ; xval = [] ; yval = [] ; zval = []
+num = 0
 
+
+# Extracting the bearing slabs from floor finishes and exterior slabs:
+for slab in slabs:
+    for RAM in slab.HasAssociations:
+        if "Finish" in RAM.RelatingMaterial.ForLayerSet.LayerSetName:
+            continue
+        if "Exterior" in RAM.RelatingMaterial.ForLayerSet.LayerSetName:
+            continue
+        slabs_structural.append(slab)
+
+# Extracting properties:
+for slab in slabs_structural:
+    num = num+1
+    slab_num.append("Slab"+str(num))
+    slab_El_name.append(slab.Name)
+    #
+    # Extracting dimensional properties:
+    for definition in slab.IsDefinedBy:
+        if definition.is_a('IfcRelDefinesByProperties'):
+            property_set = definition.RelatingPropertyDefinition
+            if property_set.Name == "PSet_Revit_Dimensions":
+                call_a = "ERROR"  ;  call_b = "ERROR"  ;  call_c = "ERROR" ; call_d = "ERROR"
+                for property in property_set.HasProperties:
+                    # Perimeter properties:
+                    if property.Name == "Perimeter":
+                        call_a = round(property.NominalValue.wrappedValue, 2)
+                    # Area Properties:
+                    elif property.Name == "Area":
+                        call_b = round(property.NominalValue.wrappedValue, 2)
+                    # Volume Properties:
+                    elif property.Name == "Volume":
+                        call_c = round(property.NominalValue.wrappedValue, 2)
+                    # Thickness properties:
+                    elif property.Name == "Thickness":
+                        call_d = round(property.NominalValue.wrappedValue, 2)
+                # Assigning result of logical test's:
+                slab_Perimeter.append(call_a)
+                slab_Area.append(call_b)
+                slab_Volume.append(call_c)
+                slab_Thickness.append(call_d)
+            #
+            # Extracting reference level:
+            if property_set.Name == "PSet_Revit_Constraints":
+                call_a = "ERROR"
+                for property in property_set.HasProperties:
+                    if property.Name == "Level":
+                        call_a = property.NominalValue.wrappedValue
+                slab_Level.append(call_a)
+    #
+    # Extracting Material:
+    for RAM in slab.HasAssociations:
+        call_a = "ERROR"
+        material = RAM.RelatingMaterial.ForLayerSet.LayerSetName
+        if material == "":
+            slab_Material.append(call_a)
+        else:
+            slab_Material.append(material)
+    #
+    # Extracting coordinates
+    call_a = "ERROR"
+    xval = round(slab.ObjectPlacement.RelativePlacement.Location.Coordinates[0],3)
+    yval = round(slab.ObjectPlacement.RelativePlacement.Location.Coordinates[1],3)
+    zval = round(slab.ObjectPlacement.RelativePlacement.Location.Coordinates[2],3)
+    # searching for errors:
+    if xval == "":
+        slab_xval.append(call_a)
+    else:
+        slab_xval.append(xval)
+    if yval == "":
+        slab_yval.append(call_a)
+    else:
+        slab_yval.append(yval)
+    if zval == "":
+        slab_zval.append(call_a)
+    else:
+        slab_zval.append(zval)
+
+
+dataframe = [slab_num, slab_El_name, slab_Level, slab_Material, slab_Thickness, slab_Area, slab_Perimeter, slab_Volume, slab_xval, slab_yval, slab_zval]
+slab_dataframe = numpy.transpose(dataframe)
 
 
 ###############################################
-#### Converting Panda to Excel             ####
+#### Creating Excel document               ####
 ###############################################
 
-import pandas as pd
-# import openpyxl   (is only used to add data to an existing Excel file)
 
-
-# Data Frame is working on a matrix basis, Index = row titles, Columns = column titles
-# every [] is a column in the matrix
-# This step must be automated so it follows the different structural elements. 
-
-#### EXAMPLE:
-# df = pd.DataFrame([[11, 21, 31], [12, 22, 32], [31, 32, 33]],
-#                    index=['one', 'two', 'three'], columns=['a', 'b', 'c'])
-
-
-# creating Data Frame for the beams
-df_beams = pd.DataFrame(
-    [[beam_El_name], [beam_Material], [beam_Lenght], [beam_placement], [beam_crossection]], 
-    index=[beam_num] , 
-    colums=['Element name', 'Material', 'Lenght', 'Placement', 'crossection']
-)
-
-# creating Data Frame for the columns
-df_columns = pd.DataFrame(
-    [[column_El_name], [column_Material], [column_Lenght], [column_placement], [column_crossection]], 
-    index=[column_num] , 
-    colums=['Element name', 'Material', 'Lenght', 'Placement', 'crossection']
-)
-
-# creating Data Frame for the walls
-df_walls = pd.DataFrame(
-    [[wall_El_name], [wall_Material], [wall_Lenght], [wall_with], [wall_placement], [wall_Voids], [wall_Voidarea] ], 
-    index=[wall_num] , 
-    colums=['Element name', 'Material', 'Lenght', 'with', 'Placement']
-)
-
-# creating Data Frame for the slaps
-df_slabs = pd.DataFrame(
-    [[slab_El_name], [slab_Material], [slab_Lenght], [slab_With], [slab_Placement]], 
-    index=[slab_num] , 
-    colums=['Element name', 'Material', 'Lenght', 'With', 'Placement']
-)
-
-# Creating Excel file consisting the Data Frames
-
-with pd.ExcelWriter('pandas_to_excel.xlsx') as writer:
-    df_beams.to_excel(writer, sheet_name='Beams', index=True, header=True)
-    df_columns.to_excel(writer, sheet_name='Columns', index=True, header=True)
-    df_walls.to_excel(writer, sheet_name='Walls', index=True, header=True)
-    df_slabs.to_excel(writer, sheet_name='Slabs', index=True, header=True)
 
 
